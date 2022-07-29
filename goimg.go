2f84d9aef2fa0a2fcdb4e3c6ed48cbfd7808e33a
a5chin <capricorn42551230@gmail.com>
2022-07-30 00:12:01 +0900

    fixed bug in GoImg

--- goimg.go
+++ goimg.go
@@ -1,6 +1,7 @@
-package goimg
+package main
 
 import (
+	"fmt"
 	"image"
 	"image/color"
 	_ "image/jpeg"
@@ -9,10 +10,11 @@ import (
 	"os"
 )
 
-type ImgConf interface {
+type ImgOperator interface {
+	String() string
 	Save()
 	Flip() GoImg
-	Gray()
+	Gray() GoImg
 }
 
 type GoImg struct {
@@ -30,29 +32,42 @@ func LoadImage(path string) (img GoImg) {
 		log.Fatal(err)
 	}
 
-	img.path = path
-
 	size := src.Bounds().Size()
-	img.width, img.height = size.X, size.Y
+	width, height := size.X, size.Y
 
-	for i := 0; i < size.X; i++ {
-		var y []color.Color
-		for j := 0; j < size.Y; j++ {
-			y = append(y, src.At(i, j))
+	img = GoImg{
+		array:  make([][]color.Color, height),
+		path:   path,
+		height: height,
+		width:  width,
+	}
+
+	for y := 0; y < height; y++ {
+		row := make([]color.Color, width)
+		for x := 0; x < width; x++ {
+			row[x] = src.At(x, y)
 		}
-		img.array = append(img.array, y)
+		img.array[y] = row
 	}
 
 	return
 }
 
+func (img *GoImg) String() (s string) {
+	s = fmt.Sprintf(
+		`path: %s, size: (%d, %d)`, img.path, img.width, img.height,
+	)
+
+	return
+}
+
 func (img *GoImg) Save(path string) {
-	xlen, ylen := len(img.array), len(img.array[0])
-	rect := image.Rect(0, 0, xlen, ylen)
+	height, width := img.height, img.width
+	rect := image.Rect(0, 0, width, height)
 	dst := image.NewNRGBA(rect)
-	for x := 0; x < xlen; x++ {
-		for y := 0; y < ylen; y++ {
-			dst.Set(x, y, img.array[x][y])
+	for y := 0; y < height; y++ {
+		for x := 0; x < width; x++ {
+			dst.Set(x, y, img.array[y][x])
 		}
 	}
 
@@ -65,18 +80,42 @@ func (img *GoImg) Save(path string) {
 	png.Encode(file, dst.SubImage(dst.Rect))
 }
 
-func (img *GoImg) Flip(direction string) {
+func (img *GoImg) Flip(direction string) (dst GoImg) {
+	height, width := img.height, img.width
+	dst = GoImg{
+		array:  make([][]color.Color, height),
+		path:   img.path,
+		height: height,
+		width:  width,
+	}
+
 	if direction == "h" {
-		for i := 0; i < len(img.array)/2; i++ {
-			z := len(img.array) - i - 1
-			img.array[i], img.array[z] = img.array[z], img.array[i]
+		for y := 0; y < height; y++ {
+			col := make([]color.Color, width)
+			for x := 0; x < width/2; x++ {
+				z := width - x - 1
+				col[x], col[z] = img.array[y][z], img.array[y][x]
+			}
+			dst.array[y] = col
 		}
 	} else if direction == "v" {
-		for _, col := range img.array {
-			for i := 0; i < len(col)/2; i++ {
-				z := len(col) - i - 1
-				col[i], col[z] = col[z], col[i]
-			}
+		for x1 := 0; x1 < height/2; x1++ {
+			x2 := height - x1 - 1
+			dst.array[x1], dst.array[x2] = img.array[x2], img.array[x1]
 		}
 	}
+
+	return
+}
+
+func (img *GoImg) Gray() (gray GoImg) {
+	height, width := img.height, img.width
+	gray = GoImg{
+		array:  make([][]color.Color, height),
+		path:   img.path,
+		height: height,
+		width:  width,
+	}
+
+	return
 }
