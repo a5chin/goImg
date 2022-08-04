d6e2d7068b32d1ed163fbd561c8a89b900982c2d
a5chin <capricorn42551230@gmail.com>
2022-08-05 01:35:49 +0900

    fixed string

--- README.md
+++ README.md
@@ -7,3 +7,42 @@
 [![Go](https://img.shields.io/badge/go-%2300ADD8.svg?style=for-the-badge&logo=go&logoColor=white)](https://go.dev/)
 
 </div>
+
+## Usage
+Currently
+- Flip
+- Grayscale 
+
+are implemented.
+
+### Flip (Holizontal, Vertical)
+```go
+package main
+
+import "goimg"
+
+func main() {
+	img := LoadImage("assets/images/cat.jpg")
+
+    // (h)olizontal or (v)ertical
+	fliped := img.Flip("v")
+	fliped.Save("assets/images/fliped.png")
+}
+```
+<img alt="fliped.png" src="assets/images/fliped.png">
+
+### GrayScale
+```go
+package main
+
+import "goimg"
+
+func main() {
+	img := LoadImage("assets/images/cat.jpg")
+	gray := img.Gray()
+	gray.Save("assets/images/gray.png")
+}
+```
+
+<img alt="gray.png" src="assets/images/gray.png">
+
--- goimg.go
+++ goimg.go
@@ -18,23 +18,26 @@ type ImgOperator interface {
 }
 
 type GoImg struct {
-	image         image.Image
-	path          string
-	height, width int
+	Image         image.Image
+	Path          string
+	Height, Width int
 }
 
 func (img *GoImg) String() string {
 	s := fmt.Sprintf(
-		"path: %s, size: (%d, %d)", img.path, img.width, img.height,
+		"path: %s, size: (%d, %d)", img.Path, img.Width, img.Height,
 	)
 
 	return s
 }
 
-func LoadImage(path string) GoImg {
+func LoadImage(path string) (img GoImg) {
 	path, _ = filepath.Abs(path)
-	file, _ := os.Open(path)
+	file, err := os.Open(path)
 	defer file.Close()
+	if err != nil {
+		log.Println("Cannot open file:", err)
+	}
 
 	src, _, err := image.Decode(file)
 	if err != nil {
@@ -44,11 +47,11 @@ func LoadImage(path string) GoImg {
 	size := src.Bounds().Size()
 	width, height := size.X, size.Y
 
-	img := GoImg{
-		image:  src,
-		path:   path,
-		height: height,
-		width:  width,
+	img = GoImg{
+		Image:  src,
+		Path:   path,
+		Height: height,
+		Width:  width,
 	}
 
 	return img
@@ -61,56 +64,60 @@ func (img *GoImg) Save(path string) {
 	}
 	defer file.Close()
 
-	png.Encode(file, img.image)
+	png.Encode(file, img.Image)
 }
 
 func (img *GoImg) Flip(direction string) GoImg {
-	canvas := image.NewRGBA(image.Rect(0, 0, img.width, img.height))
+	canvas := image.NewRGBA(
+		image.Rect(0, 0, img.Width, img.Height),
+	)
 
 	if direction == "h" {
 		// holizontal flip
-		for y := 0; y < img.height; y++ {
-			for x1 := 0; x1 < img.width/2; x1++ {
-				x2 := img.width - x1 - 1
-				canvas.Set(x1, y, img.image.At(x2, y))
-				canvas.Set(x2, y, img.image.At(x1, y))
+		for y := 0; y < img.Height; y++ {
+			for x1 := 0; x1 < img.Width/2; x1++ {
+				x2 := img.Width - x1 - 1
+				canvas.Set(x1, y, img.Image.At(x2, y))
+				canvas.Set(x2, y, img.Image.At(x1, y))
 			}
 		}
 	} else if direction == "v" {
 		// vertical flip
-		for y1 := 0; y1 < img.height/2; y1++ {
-			for x := 0; x < img.width; x++ {
-				y2 := img.height - y1 - 1
-				canvas.Set(x, y1, img.image.At(x, y2))
-				canvas.Set(x, y2, img.image.At(x, y1))
+		for y1 := 0; y1 < img.Height/2; y1++ {
+			for x := 0; x < img.Width; x++ {
+				y2 := img.Height - y1 - 1
+				canvas.Set(x, y1, img.Image.At(x, y2))
+				canvas.Set(x, y2, img.Image.At(x, y1))
 			}
 		}
 	}
 
 	dst := GoImg{
-		image:  canvas,
-		path:   img.path,
-		height: img.height,
-		width:  img.width,
+		Image:  canvas,
+		Path:   img.Path,
+		Height: img.Height,
+		Width:  img.Width,
 	}
 
 	return dst
 }
 
 func (img *GoImg) Gray() GoImg {
-	canvas := image.NewGray(image.Rect(0, 0, img.width, img.height))
+	canvas := image.NewGray(
+		image.Rect(0, 0, img.Width, img.Height),
+	)
 
-	for y := 0; y < img.height; y++ {
-		for x := 0; x < img.width; x++ {
-			canvas.Set(x, y, img.image.At(x, y))
+	for y := 0; y < img.Height; y++ {
+		for x := 0; x < img.Width; x++ {
+			canvas.Set(x, y, img.Image.At(x, y))
 		}
 	}
 
 	gray := GoImg{
-		image:  canvas,
-		path:   img.path,
-		height: img.height,
-		width:  img.width,
+		Image:  canvas,
+		Path:   img.Path,
+		Height: img.Height,
+		Width:  img.Width,
 	}
 
 	return gray
